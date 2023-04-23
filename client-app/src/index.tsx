5042baccff9e6802027f4903f8598ddbf00dca6c
er48730 <er48730@ubt-uni.net>
2023-04-23 14:33:28 +0200

    13 - Client side login and registration

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -2,23 +2,19 @@ import ReactDOM from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-calendar/dist/Calendar.css';
 import 'react-toastify/dist/ReactToastify.min.css';
-import 'react-datepicker/dist/react-datepicker.css';
+import "react-datepicker/dist/react-datepicker.css";
 import './app/layout/styles.css';
-import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
-import { StoreContext, store } from './app/stores/store';
-import { BrowserRouter } from 'react-router-dom';
-
+import { store, StoreContext } from './app/stores/store';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/routes/Routes';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <StoreContext.Provider value={store}>
-    <BrowserRouter>
-    <App />
-
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StoreContext.Provider>
 );
 
@@ -26,5 +22,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
