89ed341e1dcd859b9ae29f3cbe10d0bec4aa41cc
er48730 <er48730@ubt-uni.net>
2023-05-05 22:43:37 +0200

    Implemented chat feature with SignalR

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -179,5 +179,9 @@ export default class ActivityStore {
             runInAction(() => this.loading = false);
         }
     }
+
+    clearSelectedActivity = ()  => {
+        this.selectedActivity = undefined;
+    }
 }
 
