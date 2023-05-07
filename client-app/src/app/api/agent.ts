e9740f647a7af038c01c9b1f19df8cc797808dc7
er48730 <er48730@ubt-uni.net>
2023-05-07 20:01:32 +0200

    Implemented a follow feature from end to end

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -4,6 +4,7 @@ import { toast } from "react-toastify";
 import { store } from "../stores/store";
 import { User, UserFormValues } from "../models/user";
 import { Photo, Profile } from "../models/profile";
+import { profile } from "console";
 
 const sleep = (delay: number) => {
     return new Promise((resolve) => {
@@ -92,8 +93,11 @@ const Profiles = {
         })
     },
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
-    deletePhoto: (id: string) => requests.del(`/photos/${id}`)
-}
+    deletePhoto: (id: string) => requests.del(`/photos/${id}`),
+    updateFollowing: (username: string) => requests.post(`/follow/${username}`, {}),
+    listFollowings: (username: string, predicate: string) => 
+        requests.get<Profile[]>(`/follow/${username}?predicate=${predicate}`)
+}   
 
 
 
